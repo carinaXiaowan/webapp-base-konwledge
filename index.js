5cecca8040e7fbeea212b19b6f8c07ab51eedddd
xiaowan.chen <xiaowan.chen@weimob.com>
2020-07-14 16:48:27 +0800

    s

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('./public')); //设定静态文件的根目录
 
-app.use('/',function(req, res){
-    res.render('index', {});
+app.get('/',function(req, res){
+    res.render('rem', {});
 })
-
+app.get('/viewport',function(req, res){
+    res.render('viewport', {});
+})      
+app.get('/onepx',function(req, res){
+    res.render('onepx', {});
+})
+app.get('/eventTouch',function(req, res){
+    res.render('eventTouch', {});
+})
 app.listen(3000);
 console.info('serve is listen at 3000');
